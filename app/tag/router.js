0321e76d331228ed9d2c68adf4c5961090d54f23
LAPTOP-GCFV47F1\ASUS <yusufjojo244@gmail.com>
2023-04-11 15:16:58 +0700

    express

--- app/tag/router.js
+++ app/tag/router.js
@@ -3,6 +3,7 @@ const { police_check } = require("../../middlewares");
 const tagController = require("./controller");
 
 router.get("/tags", tagController.index);
+// router.post("/tags", tagController.store);
 router.post("/tags", police_check("create", "Tag"), tagController.store);
 router.put("/tags/:id", police_check("update", "Tag"), tagController.update);
 router.delete("/tags/:id", police_check("delete", "Tag"), tagController.destroy);
