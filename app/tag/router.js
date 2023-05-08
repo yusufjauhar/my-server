622d59eae9990a42898bd3bc2edf0e5ec073cc1a
LAPTOP-GCFV47F1\ASUS <yusufjojo244@gmail.com>
2023-05-08 22:07:30 +0700

    ok

--- app/tag/router.js
+++ app/tag/router.js
@@ -3,6 +3,7 @@ const { police_check } = require("../../middlewares");
 const tagController = require("./controller");
 
 router.get("/tags", tagController.index);
+// router.get("/tags/:id", tagController.index);
 // router.post("/tags", tagController.store);
 router.post("/tags", police_check("create", "Tag"), tagController.store);
 router.put("/tags/:id", police_check("update", "Tag"), tagController.update);
